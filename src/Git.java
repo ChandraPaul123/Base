e3ecce6daf92b6793db21d8fc965b6de8151ab15
chandrapaul123 <chandrapaul1996@gmail.com>
2022-04-23 11:52:41 +0530

    new code

---
+++ src/Git.java
@@ -0,0 +1,15 @@
+
+public class Git {
+
+	 public static void main(String[] args) {
+		
+		 int a= 15;
+		 if (a%2!=0) {
+			 System.out.println(a+" "+" is odd number");
+			
+		}
+		 else {
+			System.out.println(a+" "+" is even number");
+		}
+	}
+}
